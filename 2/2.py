a1f0a7c123d69845a4499c9a3f4ec1b3d2625f5e
tauomicronmu <tauomicronmurs@gmail.com>
2023-12-04 10:12:33 +0000

    Add solution to Day 2 Part 1

--- 2/2.py
+++ 2/2.py
@@ -4,13 +4,16 @@ def flatten(l):
     return [item for sublist in l for item in sublist]
 
 def tally_game(game): 
+    return [tally_run(run) for run in game.split(';')]
+
+def tally_run(run): 
     count = {'red': 0, 'green': 0, 'blue': 0}     
-    steps = [step[1:].split(' ') for step in game]
-    for step in steps:
-        count[step[1]] += int(step[0])
+    for step in run.split(','):
+        amt, colour = step[1:].split(' ')
+        count[colour] += int(amt)
     return count
 
-def game_is_possible(tally): 
+def is_possible(tally): 
     return tally['red'] <= 12 and tally['green'] <= 13 and tally['blue'] <= 14
 
 #  Part 1 
@@ -19,10 +22,7 @@ with open('2-input.txt', 'r') as f:
     numbered_lines = [line.split(':') for line in lines]
     games = {} 
     for line in numbered_lines:
-        games[line[0]] = (re.sub('\n', '', ','.join(line[1].split(';'))).split(','))
+        games[line[0]] = (re.sub('\n', '', line[1]))
     tallies = [(key, tally_game(game)) for (key, game) in zip(games.keys(), games.values())]
-    ids = [int(tally[0]) for tally in tallies if not game_is_possible(tally[1])]
-    print(ids)
-    for line in [(tally, game_is_possible(tally[1])) for tally in tallies]:
-        print(line)
+    ids = [int(id) for (id, tally) in tallies if all([is_possible(t) for t in tally])]
     print(f'PART 1: {sum(ids)}')
