693986a9f1981c5649964e1c02f87cf89ef1fe21
tauomicronmu <tauomicronmurs@gmail.com>
2023-12-06 09:36:16 +0000

    Add print line for part 1

--- 6/6.py
+++ 6/6.py
@@ -1,4 +1,5 @@
 import re
+from functools import reduce
 
 def parse_line(line): 
     return [int(x) for x in line.split(":")[1].strip().split()]
@@ -14,4 +15,5 @@ with open('6-input.txt', 'r') as f:
     times = [(get_times(race), race[1]) for race in races]
    
     margins = [sum([1 for x in time[0] if x > time[1]]) for time in times]
-    print(margins) 
+    product = reduce(lambda x, y: x * y, margins) 
+    print(f'PART 1: {product}') 
