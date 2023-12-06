e6042493e5c9e2853417e82f37bc848c12266dc6
tauomicronmu <tauomicronmurs@gmail.com>
2023-12-06 09:32:30 +0000

    Add solution for 6 part 1

---
+++ 6/6.py
@@ -0,0 +1,17 @@
+import re
+
+def parse_line(line): 
+    return [int(x) for x in line.split(":")[1].strip().split()]
+
+def get_times(race): 
+    return [ (race[0] - i) * i for i in range(race[0])]
+
+with open('6-input.txt', 'r') as f: 
+    lines = f.readlines() 
+    times = parse_line(lines[0]) 
+    distances = parse_line(lines[1])
+    races = zip(times, distances)
+    times = [(get_times(race), race[1]) for race in races]
+   
+    margins = [sum([1 for x in time[0] if x > time[1]]) for time in times]
+    print(margins) 
