627e0ce8e85085eaf725efd13f304e8249a8eb3d
tauomicronmu <tauomicronmurs@gmail.com>
2023-12-06 09:48:49 +0000

    Add solution to day 6 part 2

--- 6/6.py
+++ 6/6.py
@@ -4,6 +4,9 @@ from functools import reduce
 def parse_line(line): 
     return [int(x) for x in line.split(":")[1].strip().split()]
 
+def parse_line_smushed(line): 
+    return int(''.join(line.split(":")[1].strip().split()))
+
 def get_times(race): 
     return [ (race[0] - i) * i for i in range(race[0])]
 
@@ -17,3 +20,13 @@ with open('6-input.txt', 'r') as f:
     margins = [sum([1 for x in time[0] if x > time[1]]) for time in times]
     product = reduce(lambda x, y: x * y, margins) 
     print(f'PART 1: {product}') 
+
+with open('6-input.txt', 'r') as f:
+    lines = f.readlines()
+    time = parse_line_smushed(lines[0])
+    distance = parse_line_smushed(lines[1])
+    race = (time, distance)
+    times = get_times(race)
+    
+    margin = sum([1 for time in times if time > distance])
+    print(f'PART 2: {margin}') 
